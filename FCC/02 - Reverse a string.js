64af278fdc46360a335fd92d999f3bc5bcce8bd5
thernand <ing.thernandez@gmail.com>
2021-08-24 23:52:15 -0400

    create: Reverse a string

---
+++ FCC/02 - Reverse a string.js
@@ -0,0 +1,17 @@
+/*
+Reverse a StringPassed
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+*/
+
+function reverseString(str) {
+  const arr = str.split(''); // convert string to array
+  const rev = arr.reverse(); // reverse the array
+  str = rev.join(''); // convert reversed array to string
+  return str;
+}
+
+reverseString('hello');
