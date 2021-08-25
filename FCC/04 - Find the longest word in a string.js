51ad61a22ba1b219df54acd80e45dfb52c3c83ba
Tomás Hernández Carrasco <ing.thernandez@gmail.com>
2021-08-25 00:45:20 -0400

    create: Find the longest word in a string

--- FCC/04 - Find the longest word in a string.js
+++ FCC/04 - Find the longest word in a string.js
@@ -1,9 +1,9 @@
 /*
 Find the Longest Word in a String
 Return the length of the longest word in the provided sentence.
-*/
 
 Your response should be a number.
+*/
 
 function findLongestWordLength(str) {
   const words = str.split(' ');
